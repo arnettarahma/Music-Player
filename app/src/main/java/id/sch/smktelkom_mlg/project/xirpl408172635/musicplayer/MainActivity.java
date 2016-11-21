05d02936e25b5c94802f8fd5b38d28473a860a41
Arnetta Rahma <arnettarahma@gmail.com>
2016-11-21 11:52:39 +0700

    Fourth

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl408172635/musicplayer/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl408172635/musicplayer/MainActivity.java
@@ -1,101 +1,125 @@
 package id.sch.smktelkom_mlg.project.xirpl408172635.musicplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int oneTimeOnly = 0;
+    public TextView songName, startTimeField, endTimeField;
+    private MediaPlayer mediaPlayer;
+    private double startTime = 0;
+    private double finalTime = 0;
+    private Handler myHandler = new Handler();
+    private int forwardTime = 5000;
+    private int backwardTime = 5000;
+    private SeekBar seekbar;
+    private ImageButton playButton, pauseButton;
+    private Runnable UpdateSongTime = new Runnable() {
+        @Override
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            startTimeField.setText(String.format(
+                    "%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime)
+                            - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS
+                            .toMinutes((long) startTime))));
+            seekbar.setProgress((int) startTime);
+            myHandler.postDelayed(this, 100);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        songName = (TextView) findViewById(R.id.textView4);
+        startTimeField = (TextView) findViewById(R.id.textView1);
+        endTimeField = (TextView) findViewById(R.id.textView2);
+        seekbar = (SeekBar) findViewById(R.id.seekBar1);
+        playButton = (ImageButton) findViewById(R.id.imageButton1);
+        pauseButton = (ImageButton) findViewById(R.id.imageButton2);
+        songName.setText("song.mp3");
+        mediaPlayer = MediaPlayer.create(this, R.raw.aaa);
+        seekbar.setClickable(false);
+        pauseButton.setEnabled(false);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+    private void play(View view) {
+        Toast.makeText(getApplicationContext(), "Playing Music",
+                Toast.LENGTH_SHORT).show();
+        mediaPlayer.start();
+        finalTime = mediaPlayer.getDuration();
+        startTime = mediaPlayer.getCurrentPosition();
+        if (oneTimeOnly == 0) {
+            seekbar.setMax((int) finalTime);
+            oneTimeOnly = 1;
+        }
+        endTimeField.setText(String.format(
+                "%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) finalTime))));
+        startTimeField.setText(String.format(
+                "%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) startTime)
+                        - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS
+                        .toMinutes((long) startTime))));
+        seekbar.setProgress((int) startTime);
+        myHandler.postDelayed(UpdateSongTime, 100);
+        pauseButton.setEnabled(true);
+        playButton.setEnabled(false);
+    }
+
+    public void pause(View view) {
+        Toast.makeText(getApplicationContext(), "Pausing sound",
+                Toast.LENGTH_SHORT).show();
+
+        mediaPlayer.pause();
+        pauseButton.setEnabled(true);
+        playButton.setEnabled(false);
+    }
+
+    public void forward(View view) {
+        int temp = (int) startTime;
+        if ((temp + forwardTime) <= finalTime) {
+            startTime = startTime + forwardTime;
+            mediaPlayer.seekTo((int) startTime);
         } else {
-            super.onBackPressed();
+            Toast.makeText(getApplicationContext(),
+                    "Cannot jump forward 5 seconds", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void rewind(View view) {
+        int temp = (int) startTime;
+        if ((temp - backwardTime) > 0) {
+            startTime = startTime - backwardTime;
+            mediaPlayer.seekTo((int) startTime);
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "Cannot jump backward 5 seconds", Toast.LENGTH_SHORT)
+                    .show();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 }
+
