c81c0f1fe395171da8cbd4b6e78b2777add3bc11
Zulfikri Rosyid <zulf354@gmail.com>
2016-11-22 08:41:17 +0700

    Menjalankan Audio

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl408172635/musicplayer/MainContent.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl408172635/musicplayer/MainContent.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 /**
  * Created by asus on 11/22/2016.
  */
@@ -34,5 +36,40 @@ public class MainContent extends AppCompatActivity{
                 btnStop.setEnabled(true);
             }
         });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pause();
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stop();
+            }
+        });
     }
+
+
+
+    public void stateAwal() {
+        btnPlay.setEnabled(true);
+        btnPause.setEnabled(false);
+        btnStop.setEnabled(true);
+        }
+    public void play() {
+        mp = MediaPlayer.create(this, R.raw.indonesiaraya);
+        try {
+            mp.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
